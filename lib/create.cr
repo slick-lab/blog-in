1d7b1ade48c42f827db93e4f7bee86dcceaed6a2
slick phantom <ynwghosted@icloud.com>
2026-04-06 16:12:35 +0100

    Create create.cr

---
+++ lib/create.cr
@@ -0,0 +1,20 @@
+class Create 
+  def initialize(file_name: String, content: String)
+    @file_name = file_name 
+    @content = content 
+    create_file 
+  end 
+
+  def create_file 
+    final_file = "posts/#{@file_name}"
+      spawn 
+        if File.exists?(final_file)
+           return false 
+        else
+         File.write(final_file, content)
+          return true 
+       end 
+     end 
+   end 
+end 
+    
