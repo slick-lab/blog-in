c44a0e9def889fa59ccbe872065e22356291e529
slick phantom <ynwghosted@icloud.com>
2026-04-06 16:36:03 +0100

    Create get.cr

---
+++ lib/get.cr
@@ -0,0 +1,24 @@
+class Get
+  def initialize(file_name: String)
+    @file_name = file_name
+    get_post
+  end 
+
+  def get_post
+    file = "posts/#{@file_name}"
+    if File.exists?(file)
+    content = File.read(file)
+    {
+      "name" => "@file_name",
+      "content" => content,
+      "version" => "1.0.0"
+    } 
+  else 
+    return false 
+  end 
+end 
+end 
+      
+      
+     
+    
